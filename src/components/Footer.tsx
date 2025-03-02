b1d8ac68b87539f81eb750510cf1ad6c1f78e130
open00103 <open00103@gmail.com>
2025-03-02 05:15:26 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -53,7 +53,7 @@ const Footer = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone w-4 h-4 mr-2">
                   <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
                 </svg>
-                 <a href="tel:+375445404460" className="hover:text-white">+375 44 540-44-60</a>
+                 <a href="tel:+375445404460" className="text-gray-400 hover:text-white">+375 44 540-44-60</a>
                 <span class="text-gray-400"> Станислав</span>
               </li>
               <li class="flex items-center">
@@ -61,7 +61,7 @@ const Footer = () => {
                   <rect width="20" height="16" x="2" y="4" rx="2"></rect>
                   <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
                 </svg>
-                 <a href="mailto:info@remontpro.by" className="hover:text-white">info@remontpro.by</a>
+                 <a href="mailto:info@remontpro.by" className="text-gray-400 hover:text-white">info@remontpro.by</a>
               </li>
               <li class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clock w-4 h-4 mr-2">
